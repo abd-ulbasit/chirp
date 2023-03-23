5a967b50155e6d31cdd46e0625f29e119a568c4a
abd-ulbasit <basit01035@gmail.com>
2023-03-23 20:10:04 +0500

    Post View

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,34 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { createTRPCRouter, publicProcedure } from "./../trpc";
-
+import type { User } from "@clerk/nextjs/dist/api";
+import { TRPCError } from "@trpc/server";
+const filterForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany();
+    const users = (
+      await clerkClient.users.getUserList({
+        limit: 100,
+        userId: posts.map((post) => post.authorId),
+      })
+    ).map(filterForClient);
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Could not find author for post ${post.id}`,
+        });
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
