ca263ae9eba6c184a508519af21d66ccc93d6962
abd-ulbasit <basit01035@gmail.com>
2023-04-06 03:55:36 +0500

    Profile Page's tailwind

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,17 +1,27 @@
-import { UserProfile } from "@clerk/nextjs"
-import { create } from "domain"
 import { GetStaticPaths, GetStaticPathsContext, GetStaticProps, GetStaticPropsContext, NextPage } from "next"
 import { LoadingPage } from "~/components/Loading"
 import { api } from "~/utils/api"
 import { createProxySSGHelpers } from "@trpc/react-query/ssg"
 import { appRouter } from "~/server/api/root"
+import { PageLayout } from "~/components/PageLayout"
+import Image from "next/image"
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data: user } = api.profile.getUser.useQuery({ username: username })
   if (!user) return <div>404</div>
   console.log(user);
-
+  const size = 120;
   return (
-    <div>{user.username}</div>
+    <PageLayout>
+      <div className={`relative  bg-slate-800 h-36 `} >
+        <div className={`absolute left-0 bottom-0 -mb-[60px] pl-8 `} ><Image src={user.profileImageUrl} alt={`${user.username}'s profile pic`} height={size} width={size} className="rounded-full border-4 border-black" /></div>
+        <div className="h-48" ></div>
+        <div className="py-6 px-8" >
+          <div className="font-bold text-lg" > @{user.username}</div>
+        </div>
+        <div className="w-full border-b border-slate-400 " ></div>
+      </div>
+
+    </PageLayout>
     // <UserProfile></UserProfile>
   )
 }
